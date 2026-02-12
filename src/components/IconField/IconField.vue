d282f524e90fd2224f47ed94b9a7337260f99f7f
Ryu <ryu.forthepeople@gmail.com>
2026-02-12 11:40:24 +0100

    feat: add IconField component

---
+++ src/components/IconField/IconField.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="icon-field" :class="additionalClasses">
+    <slot />
+  </div>
+</template>
+
+<style src="./IconField.scss"></style>
+
+<script setup lang="ts">
+import type { IconFieldProps } from '../../types';
+import { computed } from "vue";
+
+defineOptions({ name: 'FtpIconField' });
+
+const props = withDefaults(defineProps<IconFieldProps>(), {});
+
+const additionalClasses = computed(() =>
+  [].filter(Boolean).join(" ")
+);
+</script>
